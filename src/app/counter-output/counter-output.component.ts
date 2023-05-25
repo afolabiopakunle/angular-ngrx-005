32d4e004adfd1c1b1c0f7a2988e5627dedc3988f
afolabiopakunle <opakunleafolabi@gmail.com>
2023-05-25 09:48:32 +0100

    check counter output

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-counter-output',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterOutputComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<{counter: {counter: number}}>) { }
 
   ngOnInit() {
   }
