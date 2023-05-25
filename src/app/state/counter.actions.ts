f140bcbeb8a715373914e6ef209604423d86f4fe
afolabiopakunle <opakunleafolabi@gmail.com>
2023-05-25 08:19:18 +0100

    error resolved

--- src/app/state/counter.actions.ts
+++ src/app/state/counter.actions.ts
@@ -1,4 +1,4 @@
-import { createAction } from "@ngrx/store/src/action_creator";
+import { createAction } from "@ngrx/store";
 
 export const increment = createAction('increment')
 export const decrement = createAction('decrement')
